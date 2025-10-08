cddf1be53d062fc7fd2f63f44ad1fcbb20c58d39
Bayu Prana <myselfidentity.bayuprana@gmail.com>
2025-10-08 10:31:28 +0000

    oke coba berikan dia rounded untuk bagian bawahnya

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Services from '@/components/home/Services';
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-4">
+    <div className="container mx-auto px-4 rounded-b-3xl bg-background">
       <Hero />
       <Projects />
       <Services />
